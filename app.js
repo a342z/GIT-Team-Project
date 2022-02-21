f5748a666a7500a3ea683d85ceea5c7a0b57abb1
reemapdlhamed <reemapdlhamed@gmail.com>
2022-02-21 18:55:03 +0200

    hey

--- app.js
+++ app.js
@@ -2,32 +2,36 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const body_parser = require("body-parser");
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 const multer = require("multer");
 const path = require("path");
 
 //image vars
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        // console.log(path.join(__dirname, "images"));
-        cb(null, path.join(__dirname, "images"))
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toLocaleDateString().replace(/\//g, "-") + "-" + file.originalname)
-    }
-})
+  destination: (req, file, cb) => {
+    // console.log(path.join(__dirname, "images"));
+    cb(null, path.join(__dirname, "images"));
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toLocaleDateString().replace(/\//g, "-") +
+        "-" +
+        file.originalname
+    );
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype == "image/jpeg" ||
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/png"
-    )
-        cb(null, true)
-    else
-        cb(null, false)
-
-}
+  if (
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/png"
+  )
+    cb(null, true);
+  else cb(null, false);
+};
 const mongoose = require("mongoose");
 
 const authenticationRouter = require("./routers/authenticationRouter");
@@ -35,21 +39,20 @@ const speakerRouter = require("./routers/speakerRouter");
 const studentRouter = require("./routers/studentRouter");
 const eventRouter = require("./routers/eventRouter");
 
-
 //Connecting to Database
-mongoose.connect(process.env.DB_URL)
-    .then(() => {
-        console.log("database connected");
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("database connected");
 
-        app.listen(process.env.PORT_NUMBER , () => {
-            console.log("I am Listenining .......")
-        });
-
-    })
-
-    .catch(error => {
-        console.log("DB Problem");
+    app.listen(process.env.PORT_NUMBER, () => {
+      console.log("I am Listenining .......");
     });
+  })
+
+  .catch((error) => {
+    console.log("DB Problem");
+  });
 
 //Middlewares
 // app.use(morgan(":method :url"));
@@ -64,8 +67,8 @@ app.use(cors());
 //     next();
 // })
 
-app.use("/images",express.static(path.join(__dirname, "images")))
-app.use(multer({storage, fileFilter}).single("image"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(multer({ storage, fileFilter }).single("image"));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
 
@@ -75,20 +78,16 @@ app.use(speakerRouter);
 app.use(studentRouter);
 app.use(eventRouter);
 
-
 //Not found MW
 app.use((request, response) => {
-    response.status(404).json({ data: "Not Found" });
-})
+  response.status(404).json({ data: "Not Found" });
+});
 
 //Error MW
-app.use((error, request, response, next) => {  
-    let status = error.status || 500;
-    response.status(status).json({ Error: error + "" });
-})
-
+app.use((error, request, response, next) => {
+  let status = error.status || 500;
+  response.status(status).json({ Error: error + "" });
+});
 
 console.log("adel");
-
-
-console.log("ahmed kamal");
+console.log("reem");
